0abe78bf16f8896dcae7502581d2e6dcd89de4fe
Gabriel Ami <gabriel@simpleg.eu>
2023-01-10 13:37:45 +0200

    f missing comma from json request

--- src/storage/elements/amqp_definition.rs
+++ src/storage/elements/amqp_definition.rs
@@ -1,5 +1,8 @@
-use crate::error::Error;
+use cooplan_amqp_api_consumer::error::Error;
+use cooplan_amqp_api_consumer::output::read_from_stream;
+use cooplan_amqp_api_consumer::rpc::send_request_and_wait_for_response;
 use cooplan_definitions_lib::definition::Definition;
+use cooplan_lapin_wrapper::config::api_consumer::ApiConsumer;
 use lapin::Channel;
 use std::sync::Arc;
 use tokio::sync::oneshot::Sender;
@@ -8,34 +11,49 @@ pub async fn find_by_version(
     version: String,
     channel: Arc<Channel>,
     token: String,
+    api_consumer: Arc<ApiConsumer>,
     replier: Sender<Result<Definition, Error>>,
 ) -> Result<(), Error> {
+    const API_CONSUMER_INPUT_ID: &str = "definition_by_version";
+
     let request = find_by_version_request(&version, &token);
 
-    const AMQP_QUEUE_NAME: &str = "definition";
+    send_request_and_wait_for_response::<Definition>(
+        api_consumer,
+        API_CONSUMER_INPUT_ID,
+        channel,
+        request,
+        replier,
+    )
+    .await?;
 
     Ok(())
 }
 
-fn find_by_version_request(version: &str, token: &str) -> String {
-    format!(
+fn find_by_version_request(version: &str, token: &str) -> Vec<u8> {
+    let json_request = format!(
         "{{\
             \"header\": {{
                 \"element\": \"definition\",
-                \"action\": \"get\"
+                \"action\": \"get\",
                 \"token\": \"{}\" 
             }},
             \"version\": \"{}\"
         }}",
         token, version
-    )
+    );
+
+    json_request.into_bytes()
 }
 
 pub async fn find_latest(
     channel: Arc<Channel>,
+    api_consumer: Arc<ApiConsumer>,
     replier: Sender<Result<Definition, Error>>,
 ) -> Result<(), Error> {
-    const AMQP_QUEUE_NAME: &str = "latest_definition";
+    const API_CONSUMER_OUTPUT_ID: &str = "latest_definition";
+
+    read_from_stream(api_consumer, API_CONSUMER_OUTPUT_ID, channel, replier).await?;
 
     Ok(())
 }
